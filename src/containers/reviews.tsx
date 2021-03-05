7263715865ccafbcaf6812aa28358f602590f729
Alejandra G. L. Altamirano <pandita420@MacBook-Air-de-Alejandra.local>
2021-03-04 19:34:41 -0500

    container review

---
+++ src/containers/reviews.tsx
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react'
+import ReviewCard from 'components/review-card'
+import { DrawerContext } from 'contexts/drawer/drawer.provider'
+import { useCart } from 'contexts/cart/cart.provider'
+import { useSearch } from 'contexts/search/use-search'
+import { useSearchable } from 'helpers/use-searchable'
+import NotFound from 'assets/icons/not-found'
+import { useRouter } from 'next/router'
+
+const Reviews = React.forwardRef(
+  ({ items }: any, ref: React.RefObject<HTMLDivElement>) => {
+    const { dispatch } = useContext(DrawerContext)
+
+    const { searchTerm } = useSearch()
+    const searchableItems = useSearchable(items, searchTerm, (item) => [
+      item.name
+    ])
+
+    const { addItem, removeItem, getItem } = useCart()
+
+    const router = useRouter()
+    const showDetails = (item) => {
+      dispatch({
+        type: 'STORE_PRODUCT_DETAIL',
+        payload: {
+          item: item
+        }
+      })
+    }
+
+    return (
+      <div className="w-12 px-4" ref={ref}>
+        {searchableItems.length ? (
+          <div className="grid gap-4 grid-cols-2 mt-9 md:grid-cols-2 md:col-gap-4 md:row-gap-4 lg:grid-cols-3 xxl:grid-cols-3 xxl:col-gap-4 xxl:row-gap-4 2xxl:grid-cols-4 large:grid-cols-5  ">
+            {searchableItems.map((item) => (
+              <ReviewCard item={state.item}/>
+            ))}
+          </div>
+        ) : (
+          <div className="pt-10px md:pt-40px lg:pt-20px pb-40px">
+            <NotFound width="100%" />
+            <h3 className="text-24px text-gray-900 font-bold mt-35px mb-0 text-center">
+              No product found :(
+            </h3>
+          </div>
+        )}
+      </div>
+    )
+  }
+)
+
+export default Reviews
