636e3e35b668cc02e4aad2f07c650ffdc84161e0
Alejandra G. L. Altamirano <pandita420@MacBook-Air-de-Alejandra.local>
2021-03-05 21:41:48 -0500

    NOW IS TRUE fixing redirect

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Layout from 'containers/layout/layout'
-import ProductDetailComponent from 'components/Product/product-detail'
+import ProductDetailComponent from 'components/product-detail'
 import ReviewForm from 'components/Forms/review'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
@@ -14,8 +14,17 @@ import Counter from 'components/counter'
 import ReviewCard from 'components/review-card'
 import Reviews from 'containers/reviews'
 import { getProduct } from 'helpers/get-product'
-
+import { getProducts } from 'helpers/get-products'
+import * as config from '../../config'
 export default function ProductDetail ({ product }) {
+  const router = useRouter()
+
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
   const reviews = product.data.reviews
   // console.log('reviews', product.data.reviews)
   const { t } = useTranslation('products')
@@ -49,16 +58,49 @@ export default function ProductDetail ({ product }) {
  </Layout>
   )
 }
-export async function getServerSideProps ({ locale, query }) {
-  const product = await getProduct(query.id)
-  if (!product) {
-    return null
-  }
 
+export async function getStaticPaths () {
+  // Call an external API endpoint to get posts
+  const res = await getProducts()
+
+  // Get the paths we want to pre-render based on posts
+  const paths = res.data.map((product) => ({
+    params: { id: product.id.toString() }
+  }))
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps ({ params }) {
+  console.log('params', params)
+  // const res = await getProduct(params.id)
+  // console.log('segundo res', res.json())
+  const res = await fetch(`${config.API_URL}${config.API_VERSION}/products/${params.id}`)
+  // console.log('segundo res', res.json())
+  const product = await res.json()
+  if (!params.locale) {
+    params.locale = 'es'
+  }
   return {
     props: {
       product,
-      ...await serverSideTranslations(locale, ['products', 'common'])
-    }
+      ...await serverSideTranslations(params.locale, ['products', 'common'])
+    } // will be passed to the page component as props
   }
 }
+
+// export async function getStaticPaths ({ locale }) {
+//   const blogId = await par.query.id
+//   const product = await getProduct(blogId)
+//   if (!product) {
+//     return null
+//   }
+
+//   return {
+//     props: {
+//       product,
+//       ...await serverSideTranslations(locale, ['products', 'common'])
+//     }
+//   }
+// }
